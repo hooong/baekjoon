50ef97cc5c596fcb50c7c0cd12bceb5ec5bdd769
hooong <tjrwns0529@gmail.com>
2019-06-24 09:10:12 +0000

    2448

--- 2448.py
+++ 2448.py
@@ -29,9 +29,18 @@
 import math
 
 # 가장작은 삼각형 하나
-star = ["  *   "," * *  ","***** "]
+star = ["  *   ", " * *  ", "***** "]
 
 def makestar(shift):
-    
-
-
+	c = len(star)
+	for i in range(c):
+		star.append(star[i] + star[i])
+		star[i] = ("   " * shift + star[i] + "   " * shift)
+		
+n = int(input())
+k = int(math.log(int(n/3),2))
+for i in range(k):
+	makestar(int(pow(2,i)))
+	
+for i in range(n):
+	print(star[i])
