040dd3f4c653809333a95b0554b3ca81f59aa68d
hooong <tjrwns0529@gmail.com>
2019-12-29 21:03:21 +0900

    11726

---
+++ src/Baeckjun11726.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Baeckjun11726 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int dp[] = new int[n+1];
+
+        for (int i=1; i<=n; i++) {
+            if (i==1) dp[i] = 1;
+            else if (i==2) dp[i] = 2;
+            else dp[i] = (dp[i-2] + dp[i-1]) % 10007 ;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
