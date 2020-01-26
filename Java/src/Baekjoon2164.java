e5283a848c5a29432657bbea94bf3d9c86b70876
hooong <tjrwns0529@gmail.com>
2020-01-26 21:21:57 +0900

    2164

--- Java/src/Baekjoon2164.java
+++ Java/src/Baekjoon2164.java
@@ -1,58 +1,53 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 /**
  * 2164번 카드2
- * 시간초과
  */
 public class Baekjoon2164 {
-    static List<Integer> queue = new ArrayList<>();
+    static int[] queue;
+    static int n, front = 0, back = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
+        if (n==1) System.out.println(1);
+        else {
+            queue = new int[n];
 
-        for (int i=1; i<=n; i++) {
-            push(i);
+            // 짝수들만 오름차순으로 push
+            for (int i = 2; i <= n; i += 2) {
+                push(i);
+            }
+
+            // 홀수일경우 처음 수를 맨 뒤로 보냄
+            if (n % 2 != 0) {
+                push(pop());
+            }
+
+            while (size() != 1) {
+                pop();
+                push(pop());
+            }
+
+            System.out.println(pop());
         }
-
-        while (size() != 1) {
-            pop();
-            push(pop());
-        }
-
-        System.out.println(pop());
     }
 
+    // Queue
     private static void push(int num) {
-        queue.add(num);
+        queue[(++back)%n] = num;
     }
 
     private static int pop() {
-        if (queue.isEmpty()) return -1;
-        else return queue.remove(0);
+        return queue[(++front)%n];
     }
 
     private static int size() {
-        return queue.size();
+        return Math.abs(front-back);
     }
 
     private static boolean empty() {
-        return queue.isEmpty();
+        return front == back;
     }
 
-    private static int front() {
-        if (queue.isEmpty()) return -1;
-        else return queue.get(0);
-    }
-
-    private static int back() {
-        if (queue.isEmpty()) return -1;
-        else return queue.get(queue.size()-1);
-    }
 }
