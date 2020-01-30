27814c85a409c3d20260a746dd46e0089e852322
hooong <tjrwns0529@gmail.com>
2020-01-30 20:33:37 +0900

    5430

--- Python/5430.py
+++ Python/5430.py
@@ -1,3 +1,4 @@
+from collections import deque
 # 테스트 케이스 개수
 t = int(input())
 
@@ -7,9 +8,40 @@ for _ in range(t):
 
     # 배열 입력
     n = int(input())
-    arr = input()[1:2*n].split(',')    # '[]'와 ','지우고 원소들만 담음.
+    arr = deque(input()[1:-1].split(','))    # '[]'와 ','지우고 원소들만 담음.
+    if n == 0:
+        arr = deque([])
 
+    # 뒤집기 여부(T:뒤집힌상태, F:원래상태)
+    isR = False
+    # error 여부
+    no_arr = True
 
+    for i in range(len(p)):
+        # R이 나올때마다 뒤집음.
+        if p[i] == 'R':
+            isR = not isR
+        else:
+            if len(arr) == 0:
+                print('error')
+                no_arr = False
+                break
+            else:
+                # 뒤집힌 상태라면 뒤에서 제거
+                if isR:
+                    arr.pop()
+                # 원래 상태면 앞에서 제거
+                else:
+                    arr.popleft()
+    
+    if no_arr:
+        arr = list(arr)
+        if isR:
+            arr.reverse()
+
+        print('[', end='')
+        print(','.join(arr), end='')
+        print(']')
 
     # 시간초과
     # no_arr = True
