692b009964ffa2d6519247f7f18c366b00bb5135
hooong <tjrwns0529@gmail.com>
2020-02-17 02:29:55 +0900

    12015

---
+++ Python/12015.py
@@ -0,0 +1,31 @@
+# 12015번 가장 긴 증가하는 부분 수열 2
+import sys
+
+# 이분 탐색
+def lower_bound(arr, start, end, n):
+    while start < end:
+        mid = (start + end) // 2
+
+        if arr[mid] < n:
+            start = mid + 1
+        else:
+            end = mid
+    return end    
+
+# main
+n = int(input())
+a = [int(x) for x in sys.stdin.readline().split()]
+
+answer = []
+for num in a:
+    if len(answer) == 0:
+        answer.append(num)
+    
+    if answer[-1] < num:
+        answer.append(num)
+    else:
+        idx = lower_bound(answer,0,len(answer),num)
+        answer[idx] = num
+
+print(len(answer))
+
