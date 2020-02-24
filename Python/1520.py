5affa03830e73f72da2d87e7455ef2f8dff936f4
hooong <tjrwns0529@gmail.com>
2020-02-25 02:17:13 +0900

    1520

---
+++ Python/1520.py
@@ -0,0 +1,31 @@
+# 1520번 내리막 길
+import sys
+
+# dfs
+def dfs(x,y):
+    dx = [-1,0,1,0]
+    dy = [0,-1,0,1]
+
+    if x == 0 and y == 0:
+        return 1
+
+    if dp[y][x] == -1:
+        dp[y][x] = 0
+        for i in range(4):
+            nx = dx[i] + x
+            ny = dy[i] + y
+
+            if 0 <= nx and nx < m and 0 <= ny and ny < n:
+                if map[y][x] < map[ny][nx]:
+                    dp[y][x] += dfs(nx, ny)
+    return dp[y][x]
+
+
+# main
+n, m = [int(x) for x in sys.stdin.readline().split()]
+
+map = [[int(x) for x in sys.stdin.readline().split()] for _ in range(n)]
+dp = [[(-1) for _ in range(m)] for _ in range(n)]
+
+print(dfs(m-1,n-1))
+
