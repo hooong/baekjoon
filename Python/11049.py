30245e05176f2c618aee335b793da273b1e150ba
hooong <tjrwns0529@gmail.com>
2020-02-25 00:15:17 +0900

    11049

--- Python/11049.py
+++ Python/11049.py
@@ -4,12 +4,21 @@ import sys
 # main
 n = int(sys.stdin.readline())
 
-marr = [[0,0]]
+dim = [[0,0]]
 for _ in range(n):
     tmp = [int(x) for x in sys.stdin.readline().split()]
-    marr.append(tmp)
+    dim.append(tmp)
 
 dp = [[0 for _ in range(n+1)] for _ in range(n+1)]
+for dia in range(n):
+    for i in range(1, (n - dia) + 1):
+        j = i + dia
 
+        if i == j:
+            continue
+        
+        dp[i][j] = 987654321
+        for k in range(i,j):
+            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + (dim[i][0] * dim[k][1] * dim[j][1]))
 
 print(dp[1][n])
