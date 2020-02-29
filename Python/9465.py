5adfacc92204b139c09f180e6dd112119e57cb8c
hooong <tjrwns0529@gmail.com>
2020-03-01 01:56:32 +0900

    9465

--- Python/9465.py
+++ Python/9465.py
@@ -8,16 +8,18 @@ for _ in range(t):
     n = int(sys.stdin.readline())
     st = [[int(x) for x in sys.stdin.readline().split()] for _ in range(2)]
 
-    dp = [[0 for _ in range(n+1)] for _ in range(3)]
+    if n == 1:
+        print(max(st[0][0],st[1][0]))
+    else:
+        dp = [[0 for _ in range(3)] for _ in range(n+1)]
+        
+        dp[1][1] = st[0][0]
+        dp[1][2] = st[1][0]
+        for i in range(2,n+1):
+            for j in range(1,3):
+                if j == 1:
+                    dp[i][j] = max(dp[i-1][j+1],dp[i-2][j+1]) + st[j-1][i-1]
+                else:
+                    dp[i][j] = max(dp[i-1][j-1],dp[i-2][j-1]) + st[j-1][i-1]
 
-    # 1행
-    dp[1][1] = st[0][0]
-    for j in range(2,n+1):
-        dp[1][j] = dp[1][j-2] + st[0][j-1]
-
-    # 2행
-    dp[2][1] = st[1][0]
-    for j in range(2,n+1):
-        dp[2][j] = max(dp[1][j-2], dp[1][j-1]+dp[2][j-2]) + st[1][j-1]
-    
-    print(max(dp[2]))
+        print(max(dp[n]))
