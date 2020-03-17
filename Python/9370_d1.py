26e2911a885b5df63b9a5bcc62be7d53805a4594
hooong <tjrwns0529@gmail.com>
2020-03-17 16:19:49 +0900

    9370

---
+++ Python/9370_d1.py
@@ -0,0 +1,62 @@
+# 9370번 미확인 도착지
+import sys, heapq
+INF = float('inf')
+
+# dijkstra
+def dijkstra(n, s, graph):
+
+    # 최단 거리 배열
+    d = [INF] * (n+1)
+    d[s] = 0
+
+    # 최소 힙
+    hq = []
+    heapq.heappush(hq, [0,s])
+
+    while hq:
+        tmp = heapq.heappop(hq)
+        cost = tmp[0]
+        node = tmp[1]
+
+        if cost > d[node]:
+            continue
+            
+        for v in graph[node]:
+            neighbor = v[0]
+            n_cost = d[node] + v[1]
+
+            if n_cost < d[neighbor]:
+                d[neighbor] = n_cost
+                heapq.heappush(hq, [n_cost, neighbor])
+
+    return d 
+
+# main
+T = int(input())
+
+for _ in range(T):
+    n, m, t = [int(x) for x in sys.stdin.readline().split()]
+    s, g, h = [int(x) for x in sys.stdin.readline().split()]
+
+    # 그래프 입력
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a, b, d = [int(x) for x in sys.stdin.readline().split()]
+
+        if (a == g and b == h) or (a == h and b == g):
+            d -= - 0.1    # h와 g 사이의 거리
+
+        graph[a].append([b,d])
+        graph[b].append([a,d])
+
+    targets = []
+    for _ in range(t):
+        targets.append(int(input()))
+    targets.sort()
+
+    ans_d = dijkstra(n,s,graph)
+
+    for target in targets:
+        if type(ans_d[target]) == float:
+            print(target, end=' ')
+    print()
---
+++ Python/9370_d3.py
@@ -0,0 +1,71 @@
+# 9370번 미확인 도착지
+import sys, heapq
+INF = float('inf')
+
+# dijkstra
+def dijkstra(n, s, graph):
+
+    # 최단 거리 배열
+    d = [INF] * (n+1)
+    d[s] = 0
+
+    # 최소 힙
+    hq = []
+    heapq.heappush(hq, [0,s])
+
+    while hq:
+        tmp = heapq.heappop(hq)
+        cost = tmp[0]
+        node = tmp[1]
+
+        if cost > d[node]:
+            continue
+            
+        for v in graph[node]:
+            neighbor = v[0]
+            n_cost = d[node] + v[1]
+
+            if n_cost < d[neighbor]:
+                d[neighbor] = n_cost
+                heapq.heappush(hq, [n_cost, neighbor])
+
+    return d 
+
+# main
+T = int(input())
+
+for _ in range(T):
+    n, m, t = [int(x) for x in sys.stdin.readline().split()]
+    s, g, h = [int(x) for x in sys.stdin.readline().split()]
+
+    # 그래프 입력
+    graph = [[] for _ in range(n+1)]
+    for _ in range(m):
+        a, b, d = [int(x) for x in sys.stdin.readline().split()]
+        graph[a].append([b,d])
+        graph[b].append([a,d])
+
+        if (a == g and b == h) or (a == h and b == g):
+            d_hg = d    # h와 g 사이의 거리
+
+    targets = []
+    for _ in range(t):
+        targets.append(int(input()))
+    targets.sort()
+
+    # s에서 시작하는 최단경로 배열
+    d_s = dijkstra(n,s,graph)
+    # g에서 시작하는 최단경로 배열
+    d_g = dijkstra(n,g,graph)
+    # h에서 시작하는 최단경로 배열
+    d_h = dijkstra(n,h,graph)
+
+    for target in targets:
+        # g,h 사이의 도로를 지나 target까지 가는 최단경로
+        min_d = min(d_s[g] + d_hg + d_h[target], d_s[h] + d_hg + d_g[target])
+
+        if d_s[target] == min_d:
+            print(target, end=' ')
+    print()
+
+    
