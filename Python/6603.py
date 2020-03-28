e3244fbc3cc525ff8f8139d38af87d9f255c9284
hooong <tjrwns0529@gmail.com>
2020-03-28 14:05:52 +0900

    6603_DFS

--- Python/6603.py
+++ Python/6603.py
@@ -3,25 +3,39 @@ from collections import deque
 import sys, copy
 
 # bfs
-def bfs(s, k):
-    q = deque()
+# def bfs(s, k):
+#     q = deque()
     
-    for i in range(k-5):
-        q.append([s[i]])
+#     for i in range(k-5):
+#         q.append([s[i]])
 
-    while q:
-        lotto = q.popleft()
+#     while q:
+#         lotto = q.popleft()
 
-        if len(lotto) == 6:
-            for i in lotto:
-                print(i, end=' ')
-            print()
+#         if len(lotto) == 6:
+#             for i in lotto:
+#                 print(i, end=' ')
+#             print()
 
-        for num in s:
-            if num > lotto[-1]:
-                tmp = copy.deepcopy(lotto)
-                tmp.append(num)
-                q.append(tmp)
+#         for num in s:
+#             if num > lotto[-1]:
+#                 tmp = copy.deepcopy(lotto)
+#                 tmp.append(num)
+#                 q.append(tmp)
+
+# dfs
+def dfs(s, cur, pos):
+    if len(cur) == 6:
+        for i in cur:
+            print(i, end=' ')
+        print()
+        return
+
+    for i in range(pos, len(s)):
+        if pos < len(s):
+            cur.append(s[i])
+            dfs(s,cur,i+1)
+            cur.pop()
 
 # main
 while True:
@@ -34,6 +48,7 @@ while True:
         break
 
     s.sort()
-    bfs(s, k)
+    # bfs(s, k)
+    dfs(s,[],0)
     print()
     
