aa910fc2d605d112a56519b2c00c017d5a68f1ba
hooong <tjrwns0529@gmail.com>
2020-03-28 14:08:20 +0900

    6603

--- Python/6603_dfs.py
+++ Python/6603_dfs.py
@@ -1,27 +1,6 @@
 # 6603번 로또
 from collections import deque
-import sys, copy
-
-# bfs
-# def bfs(s, k):
-#     q = deque()
-    
-#     for i in range(k-5):
-#         q.append([s[i]])
-
-#     while q:
-#         lotto = q.popleft()
-
-#         if len(lotto) == 6:
-#             for i in lotto:
-#                 print(i, end=' ')
-#             print()
-
-#         for num in s:
-#             if num > lotto[-1]:
-#                 tmp = copy.deepcopy(lotto)
-#                 tmp.append(num)
-#                 q.append(tmp)
+import sys
 
 # dfs
 def dfs(s, cur, pos):
@@ -48,7 +27,6 @@ while True:
         break
 
     s.sort()
-    # bfs(s, k)
     dfs(s,[],0)
     print()
     
