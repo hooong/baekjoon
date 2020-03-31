0d6ce46084f0ad1e6bc5af1d8721df7008e1eb02
hooong <tjrwns0529@gmail.com>
2020-03-31 19:52:54 +0900

    13460

--- Python/13460.py
+++ Python/13460.py
@@ -2,34 +2,74 @@
 from collections import deque
 import sys, copy
 
-# 기울이기
-def tilt(board, dir, r, b):
+# 기울이기 bfs
+def tilt(ball_r, ball_b, hall, board):
+    visit_R = [[0 for _ in range(m)] for _ in range(n)]
 
-# bfs (board를 저장)
-def bfs(board, ball_r, ball_b):
     q = deque()
-    q.append([ball_r, ball_b])
+    q.append([ball_r,ball_b])
 
-    d = ['up', 'down', 'left', 'right']
     count = 0
+    d = [[-1,0], [0,-1], [1,0], [0,1]]  # 상하좌우
     while q:
-        if count > 10:
+        # 최대 10번만 움직일 수 있음
+        if count == 10:
             return -1
-
-        l = len(q)
-        for _ in range(l):
-            tmp = q.popleft()
-            cur_board = copy.deepcopy(tmp[0])
-            r = tmp[1]
-            b = tmp[2]
-
-            for i in range(4):
-                tilt(cur_board, d[i], r, b)
-
+        else:
+            count += 1
+           
+        r, b = q.popleft()
         
-        count += 1
+        for i in range(4):
+            dp = d[i]
+            drx = r[0]
+            dry = r[1]
+            dbx = b[0]
+            dby = b[1]
 
-    
+            while True:
+                drx += dp[0]
+                dry += dp[1]
+
+                if board[dry][drx] != '#' and visit_R[dry][drx] == 0:
+                    visit_R[dry][drx] = count
+
+                    # R이 구멍을 만나는 경우
+                    if board[dry][drx] == 'O':
+                        while board[dby][dbx] != '#':
+                            dbx += dp[0]
+                            dby += dp[1]
+                            
+                            # B도 구멍을 만나 동시에 빠짐
+                            if board[dby][dbx] == 'O':
+                                return -1
+                        # R만 구멍으로 나옴
+                        return visit_R[dry][drx]
+                    # R이 B를 만나는 경우
+                    elif dry == dby and drx == dbx:
+                        if board[dry][drx] == '#':
+                            drx -= dp[0] * 2
+                            dry -= dp[1] * 2
+                            dbx -= dp[0]
+                            dby -= dp[1]
+                            q.append([[drx,dry],[dbx,dby]])
+                            break
+
+                        while board[dby][dbx] != '#':
+                            dbx += dp[0]
+                            dby += dp[1]
+                            
+                            # R보다 B가 먼저 구멍에 빠짐.
+                            if board[dby][dbx] == 'O':
+                                return -1
+                        # R을 다시 움직임
+                        continue
+
+
+                    
+                    
+
+
 
 # main
 n, m = [int(x) for x in sys.stdin.readline().split()]   # 세로 : n, 가로 : m
@@ -43,8 +83,3 @@ for i in range(n):
             ball_r = [i,j]
         elif row[j] == 'B':
             ball_b = [i,j]
-        elif row[j] == 'O':
-            hall = [i,j]
-
-
-
