fecc29a7ffe370a0ca1258d1b481cfcce7f8df74
hooong <tjrwns0529@gmail.com>
2020-04-01 13:29:11 +0900

    1707

--- Python/1707.py
+++ Python/1707.py
@@ -1,19 +1,30 @@
 # 1707번 이분 그래프
 from collections import deque
-import sys
+import sys, copy
 
 # bfs
 def bfs(start):
-    global graph, visit
+    global graph, V, visit
 
-    q = deque([start])
+    q = deque()
+    q.append([start,1])
+
     while q:
-        v = q.popleft()
+        cur, color = q.popleft()
 
-        for neighbor in graph[v]:
-            if not visit[neighbor]:
-                visit[neighbor] = True
-                q.append(neighbor)
+        for neighbor in graph[cur]:
+            if visit[neighbor] == 0:
+                # 인접한 곳에 컬러를 변경
+                visit[neighbor] = 3 - color
+                q.append([neighbor, 3 - color])
+
+# 연결된 노드인데 같은 색이 있는지 확인
+def check(visit, graph):
+    for i in range(V):
+        for node in graph[i]:
+            if visit[i] == visit[node]:
+                return False
+    return True
 
 # main
 T = int(input())
@@ -27,15 +38,14 @@ for _ in range(T):
         graph[x-1].append(y-1)
         graph[y-1].append(x-1)
 
-    visit = [False for _ in range(V)]
-    count = 0
+    
+    visit = [0 for _ in range(V)]
     for i in range(V):
         if not visit[i]:
-            visit[i] = True
-            count += 1
+            visit[i] = 1
             bfs(i)
-    
-    if count == 2:
+
+    if check(visit, graph):
         print("YES")
     else:
         print("NO")
