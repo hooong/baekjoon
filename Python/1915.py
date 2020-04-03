ceb1d743d73c4f1488b8181fce00c5d35e39c13b
hooong <tjrwns0529@gmail.com>
2020-04-03 14:03:51 +0900

    1915

--- Python/1915.py
+++ Python/1915.py
@@ -8,26 +8,13 @@ board = []
 for _ in range(n):
     board.append(sys.stdin.readline())
 
-# 최대 한 변의 길이를 저장
-width = [[1 for _ in range(m)] for _ in range(n)]
+# 최대 한 변의 길이를 저장하는 배열
+dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+maxSide = 0
+for y in range(n):
+    for x in range(m):
+        if board[y][x] == '1':
+            dp[y+1][x+1] = min(dp[y][x], min(dp[y+1][x], dp[y][x+1])) + 1
+            maxSide = max(maxSide, dp[y+1][x+1])
 
-dx = [-1,0,-1]
-dy = [0,-1,-1]
-max_width = 1
-for y in range(2,n):
-    for x in range(2,m):
-        isSquare = True
-
-        for i in range(3):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if board[ny][nx] != '1':
-                isSquare = False
-        
-        if isSquare:
-            width[y][x] = width[y-1][x-1] + 1
-
-        max_width = max(max_width, width[y][x] ** 2)
-
-print(max_width)
+print(maxSide ** 2)
