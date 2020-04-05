693c08fcdfa53f119367d4dc02f3a3cd9c60f3cd
hooong <tjrwns0529@gmail.com>
2020-04-05 16:44:38 +0900

    10610

--- Python/10610.py
+++ Python/10610.py
@@ -1,2 +1,20 @@
+# 10610번 30
+
+# main
 n = list(input())
 
+if '0' in n:   
+    sumAll = 0
+    for num in n:
+        sumAll += int(num)
+
+    if sumAll % 3 == 0:
+        n.sort(reverse=True)
+
+        for num in n:
+            print(num, end='')
+        print()
+    else:
+        print(-1)
+else:
+    print(-1)
