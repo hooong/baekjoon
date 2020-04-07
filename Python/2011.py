c89cd1186ca7e8efd49ead92b24dd605d42346f1
hooong <tjrwns0529@gmail.com>
2020-04-07 21:20:56 +0900

    2011

--- Python/2011.py
+++ Python/2011.py
@@ -5,33 +5,21 @@ code = input()
 
 dp = [[0 for _ in range(2)] for _ in range(len(code)+1)]
 
-# 첫째 한 자리
 if int(code[0]) == 0:
-    isError = True
+    print(0)
 else:
-    dp[0][0] = 1    # 처음 두 자리로 가능할 경우를 위한 초기화
+    dp[0][0] = 1    # 처음 두 자리로 가능할 경우
     dp[1][0] = 1    # 맨 앞으로 가능한 개수
 
-# dinamic programming
-for i in range(2, len(code)+1):
-    # 암호가 잘 못 되었는지 확인.
-    isOneErorr = False
-    isTwoError = False
-    
-    # 한 자리 수의 경우
-    if int(code[i-1]) > 0:
-        dp[i][0] = sum(dp[i-1]) % 1000000
-    else:
-        isOneErorr = True
+    # dinamic programming
+    for i in range(2, len(code)+1):
+        
+        # 한 자리 수의 경우
+        if int(code[i-1]) > 0:
+            dp[i][0] = sum(dp[i-1]) % 1000000
 
-    # 두 자리 수의 경우
-    if 10 <= int(code[i-2:i]) <= 26:
-        dp[i][1] = sum(dp[i-2]) % 1000000
-    else:
-        isTwoError = True
+        # 두 자리 수의 경우
+        if 10 <= int(code[i-2:i]) <= 26:
+            dp[i][1] = sum(dp[i-2]) % 1000000
 
-    # 에러 발생
-    if isOneErorr and isTwoError:
-        break
-
-print(sum(dp[-1]) % 1000000)
+    print(sum(dp[-1]) % 1000000)
