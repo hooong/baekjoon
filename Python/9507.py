da65fb9bb31041beacbcc0d31685cc765077ad6c
hooong <tjrwns0529@gmail.com>
2020-04-08 16:34:34 +0900

    9507

---
+++ Python/9507.py
@@ -0,0 +1,17 @@
+# 9507번 Generations of Tribbles
+
+# main
+dp = [0 for _ in range(68)]
+
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4,68):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4]
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+
+    print(dp[n])
