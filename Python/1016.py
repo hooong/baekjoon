30e4a363a55d43e4d739d4e71cdd65e29b20fa73
hooong <tjrwns0529@gmail.com>
2020-04-13 22:20:04 +0900

    1016

--- Python/1016.py
+++ Python/1016.py
@@ -1,30 +1,22 @@
 # 1016번 제곱 ㄴㄴ수
-import math
-
-# 에라토스테네스 채
-def ertos(num):
-    global prime
-    arr = [True for _ in range(num+1)]
-
-    for i in range(2,num+1):
-        if arr[i]:
-            prime.append(i**2)
-            x = i * i
-            while x <= num:
-                arr[x] = False
-                x += i
 
 # main
 minNum, maxNum = map(int,input().split())
 
-prime = []
-ertos(int(math.sqrt(maxNum)))
+eratos = [False for _ in range(maxNum-minNum+2)]
 
-count = maxNum - minNum + 1
-for num in range(minNum,maxNum+1):
-    for p in prime:
-        if num % p == 0:
-            count -= 1
-            break
+cnt = maxNum - minNum + 1
+i = 2
+while i**2 <= maxNum:
+    s = minNum // (i**2)
+    if minNum % (i**2) != 0:
+        s += 1
+    
+    while s*(i**2) <= maxNum:
+        if not eratos[s*(i**2) - minNum]:
+            eratos[s*(i**2) - minNum] = True
+            cnt -= 1
+        s += 1
+    i += 1
 
-print(count)
+print(cnt)
