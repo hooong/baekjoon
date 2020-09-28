b82dc7299cb4e9300e484bde1c1f3733f7a3b1e9
홍석준 <tjrwns0529@gmail.com>
2020-09-28 18:53:45 +0900

    2294

--- Python/2294.py
+++ Python/2294.py
@@ -1,29 +1,22 @@
 # 2294번 동전 2
+import sys
 
-# main
 n, k = map(int, input().split())
 
-# 동전 가치 입력, 정렬
-coin = [0]
+coins = []
 for _ in range(n):
     tmp = int(input())
-    if not tmp in coin:
-        coin.append(tmp)
-coin.sort()
+    if not tmp in coins:
+        coins.append(tmp)
+coins.sort()
 
-dp = [[0 for _ in range(k+1)] for _ in range(len(coin))]
-dp[0][0] = 1
+dp = [sys.maxsize] * (k+1)
+dp[0] = 0
+for i in range(len(coins)):
+    for j in range(coins[i], k+1):
+        dp[j] = min(dp[j], dp[j-coins[i]]+1)
 
-for i in range(1,len(coin)):
-    dp[i][0] = 1
-    for j in range(1,k+1):
-        if (coin[i] <= j):
-            dp[i][j] += dp[]
-
-print(dp[:][1])
-# if min(dp[:][k]) == 10001:
-#     print(-1)
-# else:
-#     print(min(dp[:][k]))
-
-
+if dp[-1] == sys.maxsize:
+    print(-1)
+else:
+    print(dp[-1])
