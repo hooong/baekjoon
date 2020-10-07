bd411ea597b3659620e6c2796894832a4e4c4087
홍석준 <tjrwns0529@gmail.com>
2020-10-07 15:48:56 +0900

    9466

---
+++ Python/9466.py
@@ -0,0 +1,34 @@
+# 9566번 텀 프로젝트
+import sys
+
+sys.setrecursionlimit(111111)
+def dfs(cur):
+    global visited, cnt, done
+
+    visited[cur] = True
+    ns = students[cur-1]
+    if not visited[ns]:
+        dfs(ns)
+    else:
+        if not done[ns]:
+            i = ns
+            while i != cur:
+                cnt += 1
+                i = students[i-1]
+            cnt += 1
+    done[cur] = True
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    students = list(map(int, sys.stdin.readline().split()))
+    cnt = 0
+    done = [False] * (n+1)
+    visited = [False] * (n+1)
+
+    for i in range(1, n+1):
+        if not visited[i]:
+            dfs(i)
+
+    print(n - cnt)
