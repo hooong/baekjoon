5b9742228f05ce7b8fa56f7ddbca2f82e45a029c
홍석준 <tjrwns0529@gmail.com>
2020-11-04 00:43:08 +0900

    11660

--- Python/11660.py
+++ Python/11660.py
@@ -1,17 +1,16 @@
 # 11660번 구간 합 구하기 5
+import sys
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-board.insert(0, [0] * n)
+dp = [[0] * (n+1) for _ in range(n+1)]
 
-for i in range(1, n+1):
+for i in range(n):
     for j in range(n):
-        if j == 0:
-            board[i][j] += board[i-1][n-1]
-        else:
-            board[i][j] += board[i][j-1]
+        dp[i+1][j+1] = dp[i+1][j] + dp[i][j+1] - dp[i][j] + board[i][j]
 
 for _ in range(m):
-    x1, y1, x2, y2 = map(int, input().split())
+    x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
 
-    ans = board
+    ans = dp[x2][y2] - (dp[x1-1][y2] + dp[x2][y1-1]) + dp[x1-1][y1-1]
+    print(ans)
