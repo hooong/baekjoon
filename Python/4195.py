98ffdb8e7b7057e35059a11f9e4a5cc617dc4f96
hooong <tjrwns0529@gmail.com>
2020-12-06 01:01:06 +0900

    4195

--- Python/4195.py
+++ Python/4195.py
@@ -1,60 +1,50 @@
 # 4195번 친구 네트워크
-from collections import deque
 import sys
-input = sys.stdin.readline
 
-# 이름을 숫자로 변환
-def findPerson(name):
-    global persons, parents
-    
-    if not name in persons.keys():
-        persons[name] = len(persons)
-        parents.append(persons[name])
 
-    return persons[name]
+def set_index(f):
+    global network, size
 
-# find
+    if not f in network:
+        network[f] = f
+        size[f] = 1
+
+    return network[f]
+
+
 def find(x):
-    global parents
+    global network
 
-    while True:
-        if parents[x] == x:
-            return x
-        x = parents[x]
+    if network[x] == x:
+        return x
 
-# union
-def union(a,b):
-    global parents
+    network[x] = find(network[x])
+    return network[x]
 
-    a = find(a)
-    b = find(b)
 
-    if not a == b:
-        parents[b] = a
+def union(x, y):
+    global network
 
-# main
-t = int(input())
+    x = find(x)
+    y = find(y)
 
-for _ in range(t):
-    f = int(input())
-    
-    persons = {'':0}
-    parents = [0]
+    if not x == y:
+        if size[x] < size[y]:
+            x, y = y, x
+
+        network[y] = x
+        size[x] += size[y]
+
+    print(size[x])
+
+
+for _ in range(int(sys.stdin.readline())):
+    f = int(sys.stdin.readline())
+    network, size = {}, {}
+
     for _ in range(f):
-        n1, n2 = input().split()
+        f1, f2 = sys.stdin.readline().split()
+        f1 = set_index(f1)
+        f2 = set_index(f2)
 
-        # 이름을 숫자로 바꿈
-        p1 = findPerson(n1)
-        p2 = findPerson(n2)
-
-        # 친구 관계 형성
-        union(p1, p2)
-
-        # 친구 관계의 수
-        count = 0
-        tmp = find(p1)
-        for i in range(1,len(parents)):
-            if find(i) == tmp:
-                count += 1
-            
-        print(count)
+        union(f1, f2)
