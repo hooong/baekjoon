9369e260c420e9b29620193f2d83fc13438cc880
hooong <tjrwns0529@gmail.com>
2020-12-24 03:46:47 +0900

    5670

--- Python/5670.py
+++ Python/5670.py
@@ -24,7 +24,7 @@ class Trie:
         cur.child['*'] = True
 
 
-def find_click(l, cur, before_size):
+def find_click(l, cur):
     global cnt
 
     if len(cur.child) > 1 or l == 0:
@@ -34,7 +34,7 @@ def find_click(l, cur, before_size):
 
     for c in cur.child:
         if c != '*':
-            find_click(l+1, cur.child[c], len(cur.child))
+            find_click(l+1, cur.child[c])
 
 
 try:
@@ -46,7 +46,7 @@ try:
         for _ in range(n):
             trie.insert(stdin.readline().strip('\n'))
 
-        find_click(0, trie.head, len(trie.head.child))
+        find_click(0, trie.head)
         print("{:,.2f}".format(round((cnt / n), 2)))
 except:
     exit(0)
