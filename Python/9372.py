54a6ad5c697b019932ea5e73bf788ca2d6800072
hooong <tjrwns0529@gmail.com>
2020-12-25 17:26:04 +0900

    9372

---
+++ Python/9372.py
@@ -0,0 +1,11 @@
+# 9372번 상근이의 여행
+from sys import stdin
+
+
+for _ in range(int(stdin.readline())):
+    n, m = map(int, stdin.readline().split())
+
+    for _ in range(m):
+        a, b = map(int, stdin.readline().split())
+
+    print(n-1)
