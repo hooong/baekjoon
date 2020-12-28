3f33d8dbb64bc059686ca5070c9a89315e5b6d7a
hooong <tjrwns0529@gmail.com>
2020-12-28 23:17:30 +0900

    12738

--- Python/12738.py
+++ Python/12738.py
@@ -5,7 +5,7 @@ def lower_bound(start, end, num):
     while start < end:
         mid = (start + end) // 2
 
-        if answer[mid] < num:
+        if tmp[mid] < num:
             start = mid + 1
         else:
             end = mid
@@ -15,16 +15,33 @@ def lower_bound(start, end, num):
 n = int(input())
 arr = list(map(int, input().split()))
 
-answer = []
-for num in arr:
-    if len(answer) == 0:
-        answer.append(num)
+tmp = []
+index = [[0, 0] for _ in range(n)]
+for i in range(len(arr)):
+    num = arr[i]
+    index[i][1] = num
+
+    if len(tmp) == 0:
+        tmp.append(num)
         continue
 
-    if answer[-1] < num:
-        answer.append(num)
+    if tmp[-1] < num:
+        index[i][0] = len(tmp)
+        tmp.append(num)
     else:
-        idx = lower_bound(0, len(answer)-1, num)
-        answer[idx] = num
+        idx = lower_bound(0, len(tmp)-1, num)
+        index[i][0] = idx
+        tmp[idx] = num
 
-print(len(answer))
+answer = []
+idx = len(tmp)-1
+for i in range(len(index)-1, -1, -1):
+    if idx == -1:
+        break
+
+    if idx == index[i][0]:
+        answer.append(index[i][1])
+        idx -= 1
+
+print(len(tmp))
+print(*answer[::-1])
