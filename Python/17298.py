d3281feff13e7fe26fd1e38ad076ba7e78cb0f7f
hooong <tjrwns0529@gmail.com>
2021-01-07 18:08:39 +0900

    17298

---
+++ Python/17298.py
@@ -0,0 +1,16 @@
+# 17298번 오큰수
+from collections import deque
+
+n = int(input())
+seq = list(map(int, input().split()))
+
+oh_big = [-1] * n
+stack = deque()
+
+for i in range(n):
+    while stack and (stack[-1][0] < seq[i]):
+        tmp, idx = stack.pop()
+        oh_big[idx] = seq[i]
+    stack.append([seq[i], i])
+
+print(*oh_big)
