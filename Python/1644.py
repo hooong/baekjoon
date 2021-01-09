ba5bb3ef9baa87befdb70e28f90e371ab3e3fa43
hooong <tjrwns0529@gmail.com>
2021-01-09 16:26:19 +0900

    1644

--- Python/1644.py
+++ Python/1644.py
@@ -2,23 +2,37 @@
 
 
 # 에라토스테네스의 채
-def eratos():
-    nums = [False] * 4000001
+def eratos(n):
+    nums = [True] * (n+1)
 
-    for i in range(2, 4000001):
-        if not nums[i]:
-            j = i + i
-            while j < 4000001:
-                nums[j] = True
-                j += i
+    m = int(n ** 0.5)
+    for i in range(2, m+1):
+        if nums[i]:
+            for j in range(i+i, n+1, i):
+                nums[j] = False
 
-    primes = []
-    for i in range(2, len(nums)):
-        if not nums[i]:
-            primes.append(i)
-
-    return primes
+    return [i for i in range(2, n+1) if nums[i]]
 
 n = int(input())
+answer = 0
 
-print(eratos())
+primes = eratos(4000000)
+
+tmp = primes[0]
+i = j = 0
+while i <= j:
+    if tmp < n:
+        if j + 1 <= len(primes) - 1:
+            j += 1
+            tmp += primes[j]
+        else:
+            tmp -= primes[i]
+            i += 1
+    elif tmp > n:
+        tmp -= primes[i]
+        i += 1
+    else:
+        answer += 1
+        j += 1
+        tmp += primes[j]
+print(answer)
