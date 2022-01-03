f1ea711fadbe7308c44c655fdf9cb40db1688266
hooong <tjrwns0529@gmail.com>
2022-01-03 21:53:05 +0900

    997_find_the_town_judge.py

---
+++ leetcode/997_find_the_town_judge.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if n == 1:
+            return 1
+
+        # people[n] = [n이 믿는 사람의 수, n을 믿는 사람의 수]
+        people = [[0, 0] for _ in range(n)]
+        for t in trust:
+            a = t[0] - 1
+            b = t[1] - 1
+
+            people[a][0] += 1
+            people[b][1] += 1
+
+        for i, person in enumerate(people):
+            if person == [0, n - 1]:
+                return i + 1
+        return -1
+
+def test():
+    solution = Solution()
+
+    assert solution.findJudge(2, [[1, 2]]) == 2
+    assert solution.findJudge(3, [[1, 3], [2, 3]]) == 3
+    assert solution.findJudge(3, [[1, 3], [2, 3], [3, 1]]) == -1
