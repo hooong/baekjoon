467507a86116f4b5b3217c28858e38bcfb13ed70
hooong <tjrwns0529@gmail.com>
2022-02-09 23:46:30 +0900

    532_k-diff_pairs_in_an_array

--- leetcode/532_k-diff_pairs_in_an_array.py
+++ leetcode/532_k-diff_pairs_in_an_array.py
@@ -1,32 +1,46 @@
+from collections import Counter
 from typing import List
+
+
+# class Solution:
+#     def findPairs(self, nums: List[int], k: int) -> int:
+#         if len(nums) == 1:
+#             return 0
+#
+#         nums.sort()
+#
+#         answer = []
+#         left = 0
+#         right = 1
+#         while left < right < len(nums):
+#             diff = nums[right] - nums[left]
+#             if diff == k:
+#                 tmp = (nums[left], nums[right])
+#                 if tmp not in answer:
+#                     answer.append(tmp)
+#                 right += 1
+#             elif diff < k:
+#                 right += 1
+#             else:
+#                 left += 1
+#
+#                 if left == right and right < len(nums):
+#                     right += 1
+#
+#         return len(answer)
 
 
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        if len(nums) == 1:
-            return 0
+        hash_table = Counter(nums)
 
-        nums.sort()
-
-        answer = []
-        left = 0
-        right = 1
-        while left < right < len(nums):
-            diff = nums[right] - nums[left]
-            if diff == k:
-                tmp = (nums[left], nums[right])
-                if tmp not in answer:
-                    answer.append(tmp)
-                right += 1
-            elif diff < k:
-                right += 1
-            else:
-                left += 1
-
-                if left == right and right < len(nums):
-                    right += 1
-
-        return len(answer)
+        cnt = 0
+        for x in hash_table:
+            if k > 0 and (x+k) in hash_table:
+                cnt += 1
+            elif k == 0 and hash_table[x] > 1:
+                cnt += 1
+        return cnt
 
 
 def test():
