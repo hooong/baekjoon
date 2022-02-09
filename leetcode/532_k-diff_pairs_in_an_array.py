499fdc29c5007cf22a8eb8100b4a1d169a1aeb27
hooong <tjrwns0529@gmail.com>
2022-02-09 23:38:07 +0900

    532_k-diff_pairs_in_an_array

---
+++ leetcode/532_k-diff_pairs_in_an_array.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+    def findPairs(self, nums: List[int], k: int) -> int:
+        if len(nums) == 1:
+            return 0
+
+        nums.sort()
+
+        answer = []
+        left = 0
+        right = 1
+        while left < right < len(nums):
+            diff = nums[right] - nums[left]
+            if diff == k:
+                tmp = (nums[left], nums[right])
+                if tmp not in answer:
+                    answer.append(tmp)
+                right += 1
+            elif diff < k:
+                right += 1
+            else:
+                left += 1
+
+                if left == right and right < len(nums):
+                    right += 1
+
+        return len(answer)
+
+
+def test():
+    solution = Solution()
+
+    assert solution.findPairs([1], 0) == 0
+    assert solution.findPairs([3, 1, 4, 1, 5], 2) == 2
+    assert solution.findPairs([1, 2, 3, 4, 5], 1) == 4
+    assert solution.findPairs([1, 3, 1, 5, 4], 0) == 1
+    assert solution.findPairs([1, 1, 1, 2, 2], 0) == 2
